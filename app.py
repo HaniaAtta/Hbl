fd19acba569580aa585d8b64b40a6e0bfac736da
Hania Atta <149865201+HaniaAtta@users.noreply.github.com>
2025-01-03 07:39:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -78,8 +78,8 @@ account_type_counts = data['Account Type'].value_counts()
 fig1, ax1 = plt.subplots(figsize=(width,height))  # Smaller plot size
 ax1.pie(account_type_counts, labels=account_type_counts.index, autopct=lambda p: f'{p:.1f}%', startangle=150,
          colors=[colors['dark_blue'], colors['slate_blue'], colors['light_beige'], colors['dark_slate']],
-         textprops={'fontsize': 5})  # Adjust label font size here
-ax1.set_title('Distribution of Account Types', fontsize=8)  # Adjust title font size
+         textprops={'fontsize': 4})  # Adjust label font size here
+ax1.set_title('Distribution of Account Types', fontsize=10)  # Adjust title font size
 
 
 
